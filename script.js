2dd67cc3040a2ff63235a9e45677240fc527085c
Everton Zardo <evezardo15@gmail.com>
2022-05-20 00:19:58 -0300

    Etch-asketch project finished Added rainbow mode for user as option for pen color and stylized all the buttons to make them look prettier.

--- script.js
+++ script.js
@@ -4,10 +4,41 @@ const showValue = document.querySelector('#slider p');
 const colorPenPicker = document.querySelector('#color-picker');
 const clearButton = document.querySelector('#clear-button button');
 const confirmButton = document.querySelector('#confirm-button');
+const rgbButton = document.getElementById('rgb-button');
+const colorButton = document.getElementById('color-button');
 
+colorButton.style.transform = 'translateY(5px)';
+colorButton.style.boxShadow = '1px 3px 5px rgb(11, 13, 20)';
+
+let rgbPressed = false;
+let colorPressed = true;
 let mousePressed = false;
+
 document.body.onmousedown = () => (mousePressed = true);
 document.body.onmouseup = () => (mousePressed = false);
+
+slider.addEventListener('input', () => showValue.textContent = `Grid-size: ${slider.value} x ${slider.value}`);
+clearButton.addEventListener('click', removeDivs);
+confirmButton.addEventListener('click', removeDivs);
+rgbButton.addEventListener('mousedown', () => {
+    rgbPressed = true;
+    rgbButton.style.transform = 'translateY(5px)';
+    rgbButton.style.boxShadow = '1px 3px 5px rgb(11, 13, 20)';
+
+    colorPressed = false;
+    colorButton.style.removeProperty('transform');
+    colorButton.style.removeProperty('box-shadow');
+});
+
+colorButton.addEventListener('mousedown', () => {
+    colorPressed = true;
+    colorButton.style.transform = 'translateY(5px)';
+    colorButton.style.boxShadow = '1px 3px 5px rgb(11, 13, 20)';
+
+    rgbPressed = false;
+    rgbButton.style.removeProperty('transform');
+    rgbButton.style.removeProperty('box-shadow');
+}); 
 
 function removeDivs(){
     while (container.firstChild) {
@@ -33,14 +64,26 @@ function createDivs(size = 16){
 }
 createDivs();
 
+function rgbColor(e){
+    let red = Math.floor(Math.random() * 258);
+    let green = Math.floor(Math.random() * 258);
+    let blue = Math.floor(Math.random() * 258);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
 function changeSquareColor(e){
     if(e.type === 'mouseover' && mousePressed === false) return;
-    let colorValue = colorPenPicker.value;
+        
+    if(rgbPressed == true){
+        let rgb = rgbColor();
+        e.target.style.backgroundColor = `${rgb}`;
+    }
+    else {
+        let colorValue = colorPenPicker.value;
+        e.target.style.backgroundColor = `${colorValue}`;
+    }
 
-    e.target.style.backgroundColor = `${colorValue}`;
     e.target.style.border = "none";     
 }
 
-slider.addEventListener('input', () => showValue.textContent = `Grid-size: ${slider.value} x ${slider.value}`);
-clearButton.addEventListener('click', () => createDivs(slider.value));
-confirmButton.addEventListener('click', removeDivs);
+
